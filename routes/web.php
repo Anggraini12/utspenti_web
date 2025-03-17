81b0475980c329ac5770ab0f9fc11ffd58752d12
Anggraini12 <pentianggrainii@gmail.com>
2025-03-17 14:16:12 +0700

    halaman login

--- routes/web.php
+++ routes/web.php
@@ -25,46 +25,12 @@ Route::get('/', function () {
 //     return view('notfound');
 // });
 
-// Route :: get('mahasiswa/{nama}', function ($nama){
-//     echo "Ini Halaman Mahasiswa dengan Nama $nama";
-// });
 
-// Route::get('/hello/hello2/hellolagi', function () {
-//     echo "Hello Kamu";
-// });
-
-// Route::get('/mahasiswa', function (){
-//     $kelas ="IS62";
-//     $data = ["Putri Chantika","Kania Nabila Muntaz","Sri Maharani","Cut Putri Efrina","Penti Anggraini"];
-//     // return view('mahasiswa.index')->with('mhs','$data')->('kls',$kelas));
-//     return view('mahasiswa.index',compact('kelas','data'));
-// });
-
-// Route::get('mahasiswa', function () {
-//     $nama ="Penti Anggraini";
-//     $nilai =98;
-
-//     $nilai2 = [85,70,50,70,35,100];
-
-//     return view('mahasiswa',compact('nama','nilai','nilai2'));
-// });
-
-
-Route::get('/master', function () {
-    $data_mhs = ["Abdul","Adha","Aidil","Alif","Asfal"];
-    return view('data.master',compact('data_mhs'));
+Route::get('/beranda', function () {
+    return view('beranda.Beranda');
 });
 
-Route::get('/mahasiswa', function () {
-    $data_mhs = ["Rani","Kania","Putri","Pece","Dini"];
-    return view('data.mahasiswa',compact('data_mhs'));
+Route::get('/login', function () {
+    return view('beranda.login');
 });
 
-Route::get('/dosen', function () {
-    $data_dos = ["Ismanuddin","Mustofa Lutfi","Rita Warni","Ridha Ansari","Dzulgunar M Nasir"];
-    return view('data.dosen',compact('data_dos'));
-});
-
-Route::get('/galeri', function () {
-    return view('data.galeri');
-});
